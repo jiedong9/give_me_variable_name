de2c7f2657ac7316d739c21bd04e0431407c829b
jiedong <785934566@qq.com>
2020-07-30 17:06:37 +0800

    update .gitignore

--- dist/private.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appSecret = exports.appId = void 0;
-exports.appId = '20200512000450063';
-exports.appSecret = 'qWfzyRkTliN1Uwe3nqUs';
--- src/private.ts
+++
@@ -1,2 +0,0 @@
-export const appId = '20200512000450063';
-export const appSecret = 'qWfzyRkTliN1Uwe3nqUs';
